35e92f07926e9f9e84ce9d6f903a9f1952112927
adel13no@gmail.com <adel13no@gmail.com>
2024-02-04 14:27:21 +0330

    feat: visibility functionalities added to models and services

--- TaskManager/Services/ITaskService.cs
+++ TaskManager/Services/ITaskService.cs
@@ -5,8 +5,10 @@ namespace TaskManager.Services
     public interface ITaskService
     {
         Task<List<TaskModel>> GetTaskList();
+        Task<List<TaskModel>> GetInvisibleTaskList();
         Task<int> AddTask(TaskModel task);
         Task<int> DeleteTask(TaskModel task);
+        Task<int> MakeTaskVisibleOrInvisible(int taskId, bool isVisible);
 
     }
 }
--- TaskManager/Services/TaskService.cs
+++ TaskManager/Services/TaskService.cs
@@ -16,11 +16,11 @@ namespace TaskManager.Services
                 await _dbConnection.CreateTableAsync<TaskModel>();
             }
         }
+
         public async Task<int> AddTask(TaskModel task)
         {
             await SetUpDb();
             return await _dbConnection.InsertAsync(task);
-
         }
 
         public async Task<int> DeleteTask(TaskModel task)
@@ -32,8 +32,30 @@ namespace TaskManager.Services
         public async Task<List<TaskModel>> GetTaskList()
         {
             await SetUpDb();
-            var taskList = await _dbConnection.Table<TaskModel>().ToListAsync();
-            return taskList;
+            var visibleTaskList = await _dbConnection.Table<TaskModel>().Where(t => t.IsVisible).ToListAsync();
+            return visibleTaskList;
+        }
+
+        public async Task<List<TaskModel>> GetInvisibleTaskList()
+        {
+            await SetUpDb();
+            var invisibleTaskList = await _dbConnection.Table<TaskModel>().Where(t => !t.IsVisible).ToListAsync();
+            return invisibleTaskList;
+        }
+
+
+        public async Task<int> MakeTaskVisibleOrInvisible(int taskId, bool isVisible)
+        {
+            await SetUpDb();
+            var taskToUpdate = await _dbConnection.Table<TaskModel>().Where(t => t.TaskId == taskId).FirstOrDefaultAsync();
+
+            if (taskToUpdate != null)
+            {
+                taskToUpdate.IsVisible = isVisible;
+                return await _dbConnection.UpdateAsync(taskToUpdate);
+            }
+
+            return 0; // Task not found
         }
     }
 }
